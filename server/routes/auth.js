471e2b45ba4ee02374c2ea2c4885eb5b18ad351b
c.hmoussa <charbel.hmoussa03@gmail.com>
2026-01-25 18:43:15 +0200

    chore: add diagnostics to login route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -86,7 +86,7 @@ router.post('/login', async (req, res) => {
 
     } catch (err) {
         console.error(err.message);
-        res.status(500).send('Server error');
+        res.status(500).json({ msg: 'Server error', error: err.message });
     }
 });
 
